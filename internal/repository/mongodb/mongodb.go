eebc2598f75bb21ebd5d1eee21af613b87a9fd20
rukiamuq-hard <kusagaid@gmail.com>
2026-07-26 18:40:52 +0300

    Start adding mongo db as a chat log DB

--- internal/repository/mongodb/mongodb.go
+++ internal/repository/mongodb/mongodb.go
@@ -1,7 +1,31 @@
 package mongodb
 
 import (
-	"go.mongodb.org/mongo-driver/v2/bson"
+	"context"
+
 	"go.mongodb.org/mongo-driver/v2/mongo"
 	"go.mongodb.org/mongo-driver/v2/mongo/options"
 )
+
+type LogsDB struct {
+	client *mongo.Client
+}
+
+func New() *LogsDB {
+	return &LogsDB{}
+}
+
+func (mdb *LogsDB) ConnectDB() error {
+
+	var err error
+	mdb.client, err = mongo.Connect(options.Client().ApplyURI("mongodb://localhost:27017"))
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (mdb *LogsDB) Close() {
+	mdb.client.Disconnect(context.Background())
+}
